471cc807578a070bfb619174b1893913015090bb
nghi <vinhnghi223@gmail.com>
2015-07-18 23:11:28 +0300

    Refactored a single constant service

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,26 +1,25 @@
 angular.module("appServices",  ['geolocation'])
 
 .constant('clientConst', {
-   CLIENT_ID:'CYEMKOM4OLTP5PHMOFVUJJAMWT5CH5G1JBCYREATW21XLLSZ',
-   CLIENT_SECRET:'GYNP4URASNYRNRGXR5UEN2TGTKJHXY5FGSAXTIHXEUG1GYM2',
-   CLIENT_VERSION:"20150408"
+  CLIENT_ID:'CYEMKOM4OLTP5PHMOFVUJJAMWT5CH5G1JBCYREATW21XLLSZ',
+  CLIENT_SECRET:'GYNP4URASNYRNRGXR5UEN2TGTKJHXY5FGSAXTIHXEUG1GYM2',
+  CLIENT_VERSION:"20150408",
+  ZERO_RESULT:["No results found","","Please type in another search terms"],
+  CALM:["Enhance your calm!!!","","We are trying to figure out where are you. Please type in the search box again after a few seconds!"],
+  ERROR:["Something's wrong !!!","","Please wait or refresh the page, and remember to choose 'Share Location'"],
+  UNIT:"m"
 })
 
-.factory('resItem', function () {
-  var messageConst ={
-   ZERO_RESULT:["No results found","","Please type in another search terms"],
-   CALM:["Enhance your calm!!!","","We are trying to figure out where are you. Please type in the search box again after a few seconds!"],
-   ERROR:["Something's wrong !!!","","Please wait or refresh the page, and remember to choose 'Share Location'"]
-  }
+.factory('resItem', function (clientConst) {
 
   var ResItem = function (name,distance,address) {
     this.name= name;
     this.distance= distance;
-    this.address= address;     
+    this.address= address;
   };
 
   var resItemFactory= function (messType) {
-    var args=messageConst[messType]
+    var args=clientConst[messType]
     return new ResItem(args[0],args[1],args[2]);
   };
 
@@ -50,50 +49,47 @@ angular.module("appServices",  ['geolocation'])
     }else{
       return $http.get(baseURL+query,{transformResponse: transformLocationData}).then(successRes,errorRes) 
     }
-    
   } 
 
   function successRes(res) {return res.data}
   function errorRes(res) {return res}
   function transformLocationData(data,headerGetter){
-    console.log('headerGetter',headerGetter);
-    var unit = 'm';
+    //console.log('headerGetter',headerGetter);
     data = angular.fromJson(data);
     var transformed, venuesArray = data.response.venues;
-    console.log('venuesArray',venuesArray)
+    //console.log('venuesArray',venuesArray)
     if(venuesArray.length != 0){
-      console.log('start loop by forEach');
+      //console.log('start loop by forEach');
       transformed=venuesArray.map(function(current){
         return resItem.getInstance(
           current.name,
-          current.location.distance+unit+' away',
+          current.location.distance+clientConst.UNIT+' away',
           current.location.address + ' '+current.location.city
         );
       });
     }else{
       transformed=[resItem.getInstance(null,null,null,"ZERO_RESULT")];
     }
-    console.log('transformed',transformed)
-    return transformed;}
-
+    //console.log('transformed',transformed)
+    return transformed;
+  }
   return  {search: search};
-
 })
-  .factory("searchService", function (locationService,$timeout,resItem) {
-    var timeoutPromise;
-    var search = function (scope) {
-      var searchWords=scope.searchWords;
-      $timeout.cancel(timeoutPromise);
-      timeoutPromise = $timeout(function() {
-        locationService.search(searchWords).then(function (res) {
-          scope.responseDataArr= res;
-        }, function (res) {
-          scope.responseDataArr= res==404 ? [resItem.getInstance(null,null,null,"CALM")] : [resItem.getInstance(null,null,null,"ERROR")];
-        });
-      }, 500);
-    }
+.factory("searchService", function (locationService,$timeout,resItem) {
+  var timeoutPromise;
+  var search = function (scope) {
+    $timeout.cancel(timeoutPromise);
+    timeoutPromise = $timeout(function() {
+      locationService.search(scope.searchWords).then(function (res) {
+        scope.responseDataArr= res;
+      }, function (res) {
+        console.log("res",res)
+        scope.responseDataArr= res==404 ? [resItem.getInstance(null,null,null,"ERROR")] : [resItem.getInstance(null,null,null,"CALM")];
+      });
+    }, 500);
+  }
 
-    return {
-      search:search
-    }
-  })
+  return {
+    search:search
+  }
+})
