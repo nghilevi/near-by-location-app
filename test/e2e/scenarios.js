1dcb1c6e78ca2f118df73e4f8cee59359f4fa2a2
nghi <vinhnghi223@gmail.com>
2015-07-19 13:41:27 +0300

    Refactored tests to scenarios

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,37 @@
+/**
+ * Created by nghi on 18.7.2015.
+ */
+describe('Given the user opens the index page', function () {
+  var searchInput,nameElem,validSearchWords="sushi",invalidSearchWords="!@#$%^&*()";
+  beforeEach(function () {
+    browser.get('');
+    searchInput = element(by.model('searchWords'));
+    nameElem=element.all(by.css('.name')).first();
+  })
+  afterEach(function () {
+    searchInput.clear();
+  })
+  describe("When the user don't share location", function () {
+    it('Then there will be an error message no matter what the user types', function () {
+      searchInput.sendKeys(validSearchWords);
+      expect(nameElem.getText()).toContain("wrong");
+    });
+  })
+  describe("When the user shares location", function () {
+    beforeEach(function () {
+      element(by.buttonText("Share Location")).click();
+    })
+    describe("And the user types in a valid search words", function () {
+      it('Then there will be results', function () {
+        searchInput.sendKeys(validSearchWords);
+        expect(nameElem.getText()).toContain(validSearchWords);
+      });
+    })
+    describe("And the user type in an invalid search words", function () {
+      it('Then there will be no result', function () {
+        searchInput.sendKeys(invalidSearchWords);
+        expect(nameElem.getText()).toContain("No");
+      });
+    })
+  })
+});
