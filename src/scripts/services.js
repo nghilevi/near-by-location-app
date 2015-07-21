3cd083cf34bc611b6430a0271d15df5eafe459fa
nghi <vinhnghi223@gmail.com>
2015-07-21 13:03:19 +0300

    Added fSBaseURL service test

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,20 +1,29 @@
 angular.module("appServices",  ['appConstants','geolocation'])
 
 .factory('fSBaseURL',function(geolocation,clientConst){
-    var baseURL;
-    geolocation.getLocation().then(function(data) {
-      if(clientConst.CLIENT_VERSION){
-        baseURL = "https://api.foursquare.com/v2/venues/search" +
-          "?client_id=" + clientConst.CLIENT_ID +
-          "&client_secret=" + clientConst.CLIENT_SECRET +
-          "&v=" + clientConst.CLIENT_VERSION +
-          "&ll=" + data.coords.latitude +
-          "," + data.coords.longitude +
-          "&query=";
-      }
-    })
+    var baseUrl;
+
+    function initBaseUrl() {
+
+      geolocation.getLocation().then(function(data) {
+        if(clientConst.CLIENT_VERSION){
+          baseUrl = "https://api.foursquare.com/v2/venues/search" +
+            "?client_id=" + clientConst.CLIENT_ID +
+            "&client_secret=" + clientConst.CLIENT_SECRET +
+            "&v=" + clientConst.CLIENT_VERSION +
+            "&ll=" + data.coords.latitude +
+            "," + data.coords.longitude +
+            "&query=";
+        }
+      });
+      return baseUrl;
+    }
+
+    initBaseUrl();
+
     return{
-      getBaseURL: function () {return baseURL}
+      getBaseURL: function () {
+        return baseUrl ? baseUrl : initBaseUrl()}
     }
 })
 .factory('searchService',function($http,$q,fSBaseURL,clientConst){
@@ -42,9 +51,9 @@ angular.module("appServices",  ['appConstants','geolocation'])
   }
 
   var search = function(query){
-    var baseURL = fSBaseURL.getBaseURL();
-    if(baseURL){
-      return $http.get(baseURL+query,{transformResponse: transformLocationData}).then(successRes)
+    var baseUrl = fSBaseURL.getBaseURL();
+    if(baseUrl){
+      return $http.get(baseUrl+query,{transformResponse: transformLocationData}).then(successRes)
     }else{
       return $q.resolve([clientConst["ERROR"]]);
     }
---
+++ test/unit/servicesSpec.js
@@ -0,0 +1,93 @@
+/**
+ * Created by nghi on 21.7.2015.
+ */
+describe('Services', function () {
+  beforeEach(module('appServices'));
+  describe('fSBaseURL', function () {
+    var fSBaseURL,geolocation,clientConst,$rootScope;
+    var user_coords={
+      coords:{
+        latitude:'1',
+        longitude:'2'
+      }
+    }
+    var baseUrlBuilder = {
+      baseUrl: "https://api.foursquare.com/v2/venues/search",
+      id: function (id) {
+        this.baseUrl+="?client_id=" + id
+        return this;
+      },
+      secret: function (secret) {
+        this.baseUrl+="&client_secret=" + secret
+        return this;
+      },
+      version: function (v) {
+        this.baseUrl+="&v=" + v;
+        return this;
+      },
+      geoData: function (data) {
+        this.baseUrl+="&ll=" + data.coords.latitude +"," + data.coords.longitude +"&query="
+        return this;
+      },
+      get: function () {
+        return this.baseUrl
+      }
+    }
+
+    beforeEach(function () {
+      inject(function(_fSBaseURL_,$q,_clientConst_, _geolocation_,_$rootScope_) {
+        $rootScope=_$rootScope_;
+        clientConst = _clientConst_;
+        fSBaseURL=_fSBaseURL_;
+
+        geolocation = { //have to defind it, otherwise it wont find the getLocation fn
+          getLocation:function(){
+            return $q.resolve(user_coords);
+          }
+        }
+
+        var deferred = $q.defer();
+        deferred.resolve(user_coords);
+
+        spyOn(geolocation,"getLocation").and.returnValue(deferred.promise);
+        spyOn(fSBaseURL,"getBaseURL").and.callFake(function () {
+
+          function initBaseUrl() {
+            var baseUrl;
+            geolocation.getLocation().then(function (data) {
+              if(clientConst.CLIENT_VERSION){
+                baseUrl=baseUrlBuilder
+                  .id(clientConst.CLIENT_ID)
+                  .secret(clientConst.CLIENT_SECRET)
+                  .version(clientConst.CLIENT_VERSION)
+                  .geoData(data)
+                  .get()
+              }
+            });
+            $rootScope.$apply()
+            return baseUrl;
+          }
+          return initBaseUrl();
+        })
+      });
+
+    });
+
+    it('should contain correct client id & secret', function () {
+      var clientIDLength = 48;
+      expect(clientConst.CLIENT_ID.length).toBe(clientIDLength)
+      expect(clientConst.CLIENT_SECRET.length).toBe(clientIDLength)
+    });
+
+    it('should return a proper url', function () {
+      var baseUrl = baseUrlBuilder
+        .id(clientConst.CLIENT_ID)
+        .secret(clientConst.CLIENT_SECRET)
+        .version(clientConst.CLIENT_VERSION)
+        .geoData(user_coords)
+        .get();
+      var expectedBaseUrl = fSBaseURL.getBaseURL();
+    });
+
+  });
+});
