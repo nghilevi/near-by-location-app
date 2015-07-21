79fdc906aa5acd06a91562adf04845ec9d0e2d9e
nghi <vinhnghi223@gmail.com>
2015-07-22 01:58:57 +0300

    Added new tests to searchServiceSpec

--- test/unit/searchServiceSpec.js
+++ test/unit/searchServiceSpec.js
@@ -4,36 +4,38 @@
 describe('searchService', function () {
   var searchService;
 
-  var $http,$q,fSBaseUrl,clientConst,$httpBackend;
+  var $http,$q,fSBaseUrl,clientConst,$httpBackend,scope;
 
-  var searchWords = "sushi", notFoundSearchWords="!@#$%^&";
+  var searchWords = "sushi", notFoundSearchWords="!@#$%^&",standardRes;
 
   beforeEach(module("appServices"));
-  beforeEach(inject(function (_$http_,_$q_,_fSBaseUrl_,_clientConst_,_searchService_,_$httpBackend_) {
-    searchService=_searchService_;
-    fSBaseUrl=_fSBaseUrl_;
-    clientConst=_clientConst_;
-    $httpBackend=_$httpBackend_;
-    //$httpBackend
-    //  .when('GET', 'http://localhost/foo')
-    //  .respond(200, { foo: 'bar' });
-    //
-    //$httpBackend.flush();
-  }))
+  beforeEach(function () {
+    inject(function (_$http_,_$q_,_fSBaseUrl_,_clientConst_,_searchService_,_$httpBackend_,$rootScope) {
+      fSBaseUrl=_fSBaseUrl_;
+      clientConst=_clientConst_;
+      $httpBackend=_$httpBackend_;
+      $http=_$http_;
+      $q=_$q_;
+      scope=$rootScope.$new()
+      searchService=_searchService_;
+
+      spyOn($q,"resolve")
+      spyOn($http,"get").and.returnValue($q.when())
+    })
+  })
+
+  it('should return results when get success', function () {
+    spyOn(fSBaseUrl,"getBaseURL").and.returnValue(true);
+    searchService.search(searchWords)
+    expect($http.get).toHaveBeenCalled()
+    expect($q.resolve).not.toHaveBeenCalled()
+  });
 
   it('should return error message if baseUrl is unavailable', function () {
-    var venueList
-    spyOn(fSBaseUrl,"getBaseURL").and.returnValue(undefined)
-    searchService.search(searchWords).then(function (data) {
-      venueList=data;
-    });
-    console.log(venueList)
-    expect(venueList).toEqual([clientConst["ERROR"]])
+    spyOn(fSBaseUrl,"getBaseURL").and.returnValue(undefined);
+    searchService.search(searchWords)
+    expect($q.resolve).toHaveBeenCalledWith([clientConst["ERROR"]])
+    expect($http.get).not.toHaveBeenCalled()
   });
-  it('should return zero result message if there is no result', function () {
 
-  });
-  it('should results when get success', function () {
-
-  });
 })
