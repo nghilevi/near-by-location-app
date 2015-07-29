3008f65481ee37b9311e6807c0db69cb07ac8bf0
nghi <vinhnghi223@gmail.com>
2015-07-29 15:37:06 +0300

    Added decorator

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,19 @@
 angular.module('nearByLocationApp',
   ['ngAnimate','ngRoute','appControllers','appServices','appDirectives','appInterceptors'])
 
-.config(function ($routeProvider,$httpProvider) {
+.config(function ($routeProvider,$httpProvider,$provide) {
     $httpProvider.interceptors.push('fSClientParamsInjector','timeStampMarker')
+    $provide.decorator('geolocation', function ($delegate,$q) {
+      var _getLocation=$delegate.getLocation;
+      $delegate.getLocation = function () {
+        var defer=$q.defer()
+        _getLocation().then(function (data) {
+          defer.resolve(data.coords.latitude +"," + data.coords.longitude)
+        })
+        return defer.promise;
+      }
+      return $delegate;
+    });
     $routeProvider
     .when('/', {
       templateUrl: 'src/views/listView.html',
